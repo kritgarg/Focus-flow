56f4ffd6f6993a8cc86c448f5950b38c6693954d
kritgarg <kritg0160@gmail.com>
2025-04-15 12:11:15 +0530

    adding navbar

--- focus_flow/src/App.jsx
+++ focus_flow/src/App.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-        </Switch>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
