569b30127cb24a1d1d9d1cc4653afd76bc648157
kritgarg <kritg0160@gmail.com>
2025-04-17 01:50:24 +0530

    changing sidebar

--- focus_flow/src/App.jsx
+++ focus_flow/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Products from './pages/Products';
+import Sidee from './components/sidee';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/reports' element={<Reports />} />
-        <Route path='/products' element={<Products />} />
-      </Routes>
-    </Router>
+    <>
+      <Sidee />
+    </>
   );
 }
 
---
+++ focus_flow/src/components/sidee.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './sidee.css'
+const sidee = () => {
+  return (
+    <div>
+      <div className="sidee"> 
+      <navbar>
+        <li>DASHBOARD</li>
+        <li>TO-DO</li>
+        <li>POMODORO</li>
+        <li>CHATGPT</li>
+      </navbar>
+        </div> 
+    </div>
+  )
+}
+
+export default sidee
