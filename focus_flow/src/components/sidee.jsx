d62b30e2d38974325fa7d6f03add58e1f026e48e
kritgarg <kritg0160@gmail.com>
2025-04-17 11:48:12 +0530

    styling  sidebar

--- focus_flow/src/components/sidee.jsx
+++ focus_flow/src/components/sidee.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './sidee.css'
+import { MdOutlineDashboard } from "react-icons/md";
+import { LuListTodo } from "react-icons/lu";
+import { MdOutlineTimer } from "react-icons/md";
+import { IoChatboxOutline } from "react-icons/io5";
+import { LuFocus } from "react-icons/lu";
+import 'animate.css';
+
 const sidee = () => {
   return (
     <div>
       <div className="sidee"> 
       <navbar>
-        <li>DASHBOARD</li>
-        <li>TO-DO</li>
-        <li>POMODORO</li>
-        <li>CHATGPT</li>
+      <p className='logo' > <LuFocus /></p>
+        <li  class="hover-underline"> <MdOutlineDashboard />DASHBOARD</li>
+        <li class="hover-underline" ><LuListTodo /> TO-DO</li>
+        <li class="hover-underline" ><MdOutlineTimer /> POMODORO</li>
+        <li class="hover-underline" ><IoChatboxOutline />CHATGPT</li>
+        <li class="hover-underline" ><IoChatboxOutline />CHATGPT</li>
+        <li class="hover-underline" ><IoChatboxOutline />CHATGPT</li>
+        <li class="hover-underline" ><IoChatboxOutline />CHATGPT</li>
       </navbar>
         </div> 
     </div>
